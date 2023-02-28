60dd3e7d5bc34ece9bb6c0e89b0097e6f4eb8121
mdsultanmahmud <mdsultanmahmud.bd00@gmail.com>
2023-02-28 13:46:05 +0600

    banner done

---
+++ src/pages/home/Banner.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import banner from '../../assets/home_assets/banner.jpg'
+const Banner = () => {
+    return (
+        <div className='my-12'>
+            <img className='h-[200px]' src={banner} alt="" />
+        </div>
+    );
+};
+
+export default Banner;
